73021b9177aec5cd07e4323afcd775a6ee7e3da5
SanyaNad <sanya130812@gmail.com>
2026-09-18 20:11:36 +0300

    readme update

---
+++ README.md
@@ -0,0 +1,18 @@
+Text
+
+# Title
+### Subtitle
+
+- 1
+- 2
+- 3
+
+1. A
+2. B
+3. C
+
+`main.py`
+
+```python
+python main.py
+```
--- games.py
+++ games.py
@@ -1,5 +1,5 @@
 def show_games(games):
     print("=== Catalog of table games ===")
     for number, game in enumerate(games, start=1):
-        print(f"{number}, {game['name']}")
+        print(f"{number}. {game['name']}")
         print(f"Genre: {game['genre']}")
