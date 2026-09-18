d871fe0be027056eba2d586d5bd8ba92c3600a7d
SanyaNad <sanya130812@gmail.com>
2026-09-18 19:50:17 +0300

    Created catalog

---
+++ main.py
@@ -0,0 +1,14 @@
+from games import *
+
+games = [
+    {
+        "name" : "Carcassonne",
+        "genre" : "Strategy"
+    },
+    {
+        "name" : "Dixit",
+        "genre" : "Imagination"
+    }
+]
+
+show_games(games)
